411a5dff0719b917c6afe0be776dc252b0a1af49
arshidbaba <github.com/arshidbaba>
2020-06-30 21:04:55 +0530

    counted inversions of a large set of items

--- inversion_count.py
+++ inversion_count.py
@@ -10,13 +10,19 @@ def inverse(A):
         for j in range(i+1, len(A)):
             if A[i] > A[j]:
                 A[i], A[j] = swap(A[i], A[j])
-                print(A)
-                print(i, j)
+                # print(A)
+                # print(i, j)
                 count += 1
-    print(A)
-    print(count)
-    return
+    # print(A)
+    # print(count)
+    # print(tuple(i,j))
+    return count
 
 if __name__ == "__main__":
-    A = [2, 4, 1, 3, 5]
-    inverse(A)
+    # A = [2, 4, 1, 3, 5]
+    count = 0
+    print(count)
+    with open("IntegerArray.txt") as f:
+        A = [int(line.rstrip('\n')) for line in f]
+    count = inverse(A)
+    print("\n",count,"\n")
